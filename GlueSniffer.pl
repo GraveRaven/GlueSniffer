7c96fca3efb4795736226f8a5048697b33a2db2c
Oscar Carlsson <oscar.carlsson.87@gmail.com>
2013-03-03 21:12:32 +0100

    Added matchlist parser

--- GlueSniffer.pl
+++ GlueSniffer.pl
@@ -1,6 +1,32 @@
 #!/usr/bin/perl
 
 use v5.14;
+use Getopt::Long;
 use LWP::Simple;
 
+my $filename = "match.txt";
+
+GetOptions('f=s' => \$filename);
+
+# Parses the match list and puts it in a dictionary
+sub parse_matchlist{
+    my $filename = shift;
+    open FILE, $filename;
+
+    my %regexps;
+
+    while(my $line = <FILE>){
+        chomp $line;
+        if(length($line) > 0 && (substr $line, 0, 1) ne "#"){
+            (my $regexp, my $weigth) = split "\t", $line, 2;
+            if(eval {qr/$regexp/}){ # Make sure the regexp is valid
+                $regexps{$regexp} = 0+ $weigth; # Force the weigth to be a number
+            }
+        }
+    }
+
+    return %regexps;
+}
+
+my %regexps = parse_matchlist $filename;
 
